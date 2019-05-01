a8ebf08d1acfb15c4d91690ffb0fb2d7a7f54980
zw lin <1130120850@qq.com>
2019-05-01 23:35:07 +0800

    第三期11.02，熟悉了基本的数据类型

--- HelloWorld.go
+++ HelloWorld.go
@@ -31,6 +31,7 @@ type gopher struct { }
 type golang interface { }
 
 func main() {
+	// 整形
 	var a int = 1
 	var b uint = 1
 	var c int8 = 1
@@ -42,8 +43,17 @@ func main() {
 	var i uint32 = 1
 	var j uint64 = 1
 
-	var k rune = 1
-	var l byte = 1
+	// 别名整形
+	var k rune = 1 // int32
+	var l byte = 1 // uint8
+
+	// 浮点型
+	var m float32 = .1
+	var n float64 = 2.2
+
+	// 复数 z=a+bi（a,b均为实数）的数称为复数，其中a称为实部，b称为虚部，i称为虚数单位
+	var o complex64 = 1.11
+	var p complex128 = 2.22
 
 	fmt.Println("hello from fmt2 ")
 }
