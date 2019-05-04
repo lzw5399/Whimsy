542d593355b1d3bc98a6ca52887adf37876fa62f
zw lin <1130120850@qq.com>
2019-05-04 23:00:51 +0800

    进度:第四季done 内容:常量，枚举，运算符

---
+++ Consts.go
@@ -0,0 +1,33 @@
+// 常量&枚举
+package main
+
+import "fmt"
+
+// 星期枚举例子
+const (
+	a = 'A'
+	Monday = iota
+	Tuesday
+	Wednesday
+	Thursday
+	Friday
+	Saturday
+	Sunday
+)
+
+func main() {
+	var x byte = 'a'
+	fmt.Println(typeof(x))
+	fmt.Println(Monday)
+	fmt.Println(Tuesday)
+	fmt.Println(Wednesday)
+	fmt.Println(Thursday)
+	fmt.Println(Friday)
+	fmt.Println(Saturday)
+	fmt.Println(Sunday)
+}
+
+// 获取当前变量的类型
+func typeof(v interface{}) string {
+	return fmt.Sprintf("%T", v)
+}
---
+++ Operator.go
@@ -0,0 +1,27 @@
+// 运算符
+package main
+
+import "fmt"
+
+const(
+
+
+)
+
+func main() {
+	a := 6 & 11  // 按位与
+	b := 6 | 11  // 按位或
+	c := 6 ^ 11  // 按位异或
+	d := 6 &^ 11 // 将运算符左边数据相异的位保留，相同位清零
+	fmt.Println(a)
+	fmt.Println(b)
+	fmt.Println(c)
+	fmt.Println(d)
+
+	fmt.Println(1 << 10)
+	fmt.Println(1024 >> 10)
+
+	if a != b {
+		fmt.Println(a != b)
+	}
+}
