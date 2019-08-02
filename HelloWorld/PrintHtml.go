8aab12431c874f1cb70499e38355505f68b24a61
zhiwen <zhiwen@kooboo.cn>
2019-08-02 13:33:29 +0800

    打印网页

---
+++ HelloWorld/PrintHtml.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+func main() {
+	r, err := http.Get("https://github.com")
+	if err != nil {
+		panic(err)
+	}
+	defer r.Body.Close()
+
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("body = %s\n", string(body))
+}
