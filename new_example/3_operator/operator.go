d9cfff852a3c6193ca15bbf38a475e5db418e22b
zhiwen <zhiwen@kooboo.cn>
2019-08-18 00:09:34 +0800

    bit operator

--- new_example/3_operator/operator.go
+++ new_example/3_operator/operator.go
@@ -6,10 +6,16 @@ import (
 
 func main() {
 	a := 1 << 10 // 左移，乘以2的n次方
-
 	fmt.Println(a)
 
-	fmt.Println(^2)
+	fmt.Println(^2) // 未知是干嘛的
 
-	d :=true&false
+	d := 6 & 11 //位运算符之 【按位与运算符】
+	e := 6 | 11 //位运算符之 【按位或运算符】
+	f := 6 ^ 11 //位运算符之 【按位异或运算符】
+	g := 6 &^ 11 //位运算符之 【按位零运算符】
+	fmt.Println(d)
+	fmt.Println(e)
+	fmt.Println(f)
+	fmt.Println(g)
 }
