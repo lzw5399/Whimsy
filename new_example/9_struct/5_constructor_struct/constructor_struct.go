b8c77baf1578b44760107331cc30986ce42a54d3
zhiwen <zhiwen@kooboo.cn>
2019-08-22 18:14:40 +0800

    constructor & Nesting

--- new_example/9_struct/5_constructor_struct/constructor_struct.go
+++ new_example/9_struct/5_constructor_struct/constructor_struct.go
@@ -2,8 +2,16 @@ package main
 
 import (
 	"fmt"
+	ptest "github.com/zhiwen-kooboo/GolangDemo/new_example/9_struct/package_for_test"
 )
 
 func main() {
-	fmt.Println()
+	// 测试父struct Cat
+	t := ptest.NewCat("zhangsan", "math teacher")
+	fmt.Printf("%T\r\n", t)
+
+	// 测试子struct BlackCat
+	b := ptest.NewBlackCat("black1")
+	fmt.Println(b.Color, b.Name)
+	fmt.Printf("%T\r\n", b)
 }
---
+++ new_example/9_struct/package_for_test/package_struct.go
@@ -0,0 +1,32 @@
+package package_for_test
+
+type Cat struct {
+	Name  string
+	Color string
+}
+
+// 模拟一个【子类】
+// Note: 这里的【父struct】不需要给【成员名】，然后就可以支持以下【两种】写法
+// 1、【blackCat.Name】 2、【blackCat.Cat.Name】
+// 如果给了【成员名】，则只可以用这种写法
+// 1、【blackCat.成员名.Name】
+type BlackCat struct {
+	Cat // 不写成员名，这种用法叫【嵌入】
+}
+
+// 由于go不支持重载，所以这边构造函数的名字不能一样啊
+func NewCatByName(name string) *Cat {
+	return &Cat{Name: name}
+}
+
+// 父struct构造函数
+func NewCat(name string, color string) *Cat {
+	return &Cat{Name: name, Color: color}
+}
+
+// 子struct构造函数
+func NewBlackCat(color string) *BlackCat {
+	cat := new(BlackCat) // 这种写法等同于 cat := &BlackCat{}
+	cat.Color = color
+	return cat
+}
