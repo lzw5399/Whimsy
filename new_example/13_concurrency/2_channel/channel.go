77eb8c7bc05989311d7c687eb66c676aaecb048c
zhiwen <zhiwen@kooboo.cn>
2019-08-25 12:56:15 +0800

    channel & get channel value via for range

---
+++ new_example/13_concurrency/2_channel/channel.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	// 创建一个channel对象，里面存的数据是bool
+	c := make(chan bool)
+
+	go func() {
+		fmt.Println("run run run")
+		// 将true存进channel, 这里的关键是因为我们定义了一个chan，并且在【主线程】外取chan的值
+		// 所以goroutine里面【主线程】会等待chan被赋值，再所以【gogogo】会被稳定打印
+		c <- true
+	}()
+	<-c // 取出channel
+	close(c)
+}
---
+++ new_example/13_concurrency/3_for_range_channel/forRangeChannel.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	// 创建一个channel对象，里面存的数据是bool
+	c := make(chan [2]int)
+
+	go func() {
+		fmt.Println("run run run")
+		// 将true存进channel, 这里的关键是因为我们定义了一个chan，并且在【主线程】外取chan的值
+		// 所以goroutine里面【主线程】会等待chan被赋值，再所以【gogogo】会被稳定打印
+		c <- [2]int{22, 33}
+		close(c)
+	}()
+
+	// 在迭代的过程中一直在等待chan，有一个值进去，才会打印
+	for v := range c {
+		fmt.Println(v)
+	}
+}
