2fb5993eab080643775120186693d30337e022eb
zhiwen <zhiwen@kooboo.cn>
2019-08-28 17:30:49 +0800

    func init

---
+++ new_example/8_func/init/init.go
@@ -0,0 +1,8 @@
+package main
+
+import "github.com/zhiwen-kooboo/GolangDemo/new_example/8_func/package_for_test"
+
+func main() {
+	package_for_test.Hello()
+	package_for_test.Hello2()
+}
---
+++ new_example/8_func/package_for_test/package.go
@@ -0,0 +1,15 @@
+package package_for_test
+
+import "fmt"
+
+func init() {
+	fmt.Println("inited")
+}
+
+func Hello(){
+	fmt.Println("hello")
+}
+
+func Hello2(){
+	fmt.Println("hello2")
+}
