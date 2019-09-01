d1b8c1190372649d311527635f80beeacb97815e
zhiwen <zhiwen@kooboo.cn>
2019-09-01 21:47:10 +0800

    request redis

--- bili_code/2_redis/redis.go
+++ bili_code/2_redis/redis.go
@@ -1,5 +1,17 @@
 package main
 
+import (
+	"fmt"
+	"github.com/gomodule/redigo/redis"
+)
+
 func main() {
-	
+	fmt.Println("start request redis...")
+	conn, err := redis.Dial("tcp", "123.207.67.80:6379,password=redis@pwd,poolsize=5")
+	if err != nil{
+		fmt.Println("request redis failed",err)
+		return
+	}
+	defer conn.Close()
+
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/zhiwen-kooboo/GolangDemo
 
 go 1.12
 
-require github.com/nsf/gocode v0.0.0-20190302080247-5bee97b48836 // indirect
+require github.com/gomodule/redigo v2.0.0+incompatible
